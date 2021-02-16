18d47d371359a7096e67058755f63e1d86b014b9
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-02-15 19:53:10 -0700

    Updated dev

--- Develop/seeds.sql
+++ Develop/seeds.sql
@@ -1,18 +1,11 @@
-DROP DATABASE IF EXISTS employee_trackerdb;
-CREATE database employee_trackerdb;
-
 USE employee_trackerdb;
 
-CREATE TABLE department (
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (position)
-);
-
-CREATE TABLE role (
+CREATE TABLE employee_role (
   title VARCHAR(30) NULL,
   salary DECIMAL(10,4) NULL,
-  department_id INT NULL,
+  employee_role INT NULL,
 );
+
 CREATE TABLE employee (
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
@@ -20,4 +13,5 @@ CREATE TABLE employee (
   manager_id INT,
 );
 
-
+SELECT * FROM employeer_role;
+select * from employee;
